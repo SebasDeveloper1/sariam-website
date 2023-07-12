d941c03a456dce12c168644d7f50ea02a3197d6d
Sebastian-Pedroza <sebaspedroza.developer@gmail.com>
2023-07-11 20:21:18 -0500

    :lipstick: Added loadingSkeleton
    Added Skeleton loading in events section and services section.

--- components/EventCard/EventCard.tsx
+++ components/EventCard/EventCard.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState, MouseEvent, CSSProperties } from 'react';
+import { useState, useEffect, MouseEvent, CSSProperties } from 'react';
 import Image from 'next/image';
 import { Event } from '@/api/generated/graphql';
 import Modal from '@/containers/Modal/Modal';
@@ -11,7 +11,14 @@ const imageStyle: CSSProperties = {
 };
 export default function EventCard({ event }: { event: Event }) {
   const { mediaCollection } = event;
+  const [loading, setLoading] = useState<boolean>(true);
   const [eventModalOpen, setEventModalOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(!loading);
+    }, 500);
+  }, []);
 
   const handleClick = (e: MouseEvent) => {
     e.preventDefault();
@@ -20,24 +27,28 @@ export default function EventCard({ event }: { event: Event }) {
 
   return (
     <>
-      <button
-        type="button"
-        className="overflow-hidden col-span-1 md:col-span-3 relative w-full h-64 rounded-2xl bg-slate-600"
-        onClick={handleClick}
-      >
-        <Image
-          src={mediaCollection?.items[0]?.url || ''}
-          alt="evento"
-          fill={true}
-          style={imageStyle}
-          loading="lazy"
-          quality={100}
-          placeholder="blur"
-          blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="hover:scale-105 cursor-pointer"
-        />
-      </button>
+      {loading ? (
+        <div className="overflow-hidden col-span-1 md:col-span-3 relative w-full h-64 rounded-2xl bg-gray-700 animate-pulse" />
+      ) : (
+        <button
+          type="button"
+          className="overflow-hidden col-span-1 md:col-span-3 relative w-full h-64 rounded-2xl bg-slate-600"
+          onClick={handleClick}
+        >
+          <Image
+            src={mediaCollection?.items[0]?.url || ''}
+            alt="evento"
+            fill={true}
+            style={imageStyle}
+            loading="lazy"
+            quality={100}
+            placeholder="blur"
+            blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            className="lg:hover:scale-105 cursor-pointer"
+          />
+        </button>
+      )}
       {eventModalOpen && (
         <Modal
           isOpen={eventModalOpen}
--- components/ServiceCard/ServiceCard.tsx
+++ components/ServiceCard/ServiceCard.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { MouseEvent, useState } from 'react';
+import { MouseEvent, useState, useEffect } from 'react';
 import { FaAngleRight } from 'react-icons/fa';
 import { ServiceCardProps } from './ServiceCard.model';
 import Modal from '@/containers/Modal/Modal';
@@ -7,7 +7,14 @@ import ServiceModal from '@/containers/ServicesSection/ServiceModal/ServiceModal
 
 export default function ServiceCard({ data }: ServiceCardProps): JSX.Element {
   const { bgCard, icon, title, cardDescription } = data;
+  const [loading, setLoading] = useState<boolean>(true);
   const [serviceModalOpen, setServiceModalOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
 
   const handleClick = (e: MouseEvent) => {
     e.preventDefault();
@@ -16,27 +23,31 @@ export default function ServiceCard({ data }: ServiceCardProps): JSX.Element {
 
   return (
     <>
-      <button
-        className="col-span-6 md:col-span-3 xl:col-span-2 overflow-hidden w-full h-full min-h-[7rem] rounded-2xl shadow-lg bg-gray-800 hover:bg-gray-700"
-        onClick={handleClick}
-      >
-        <div
-          className={`w-full h-full ${bgCard} bg-no-repeat bg-right bg-contain`}
+      {loading ? (
+        <div className="col-span-6 md:col-span-3 xl:col-span-2 overflow-hidden w-full h-full min-h-[7rem] rounded-2xl shadow-lg bg-gray-800 animate-pulse" />
+      ) : (
+        <button
+          className="col-span-6 md:col-span-3 xl:col-span-2 overflow-hidden w-full h-full min-h-[7rem] rounded-2xl shadow-lg bg-gray-800 hover:bg-gray-700"
+          onClick={handleClick}
         >
-          <div className="relative flex justify-center items-center gap-5 w-full h-full p-6 text-start bg-gray-900/25">
-            <span className="flex items-center justify-center aspect-square rounded-full bg-lime-50 text-5xl text-lime-500">
-              {icon}
-            </span>
-            <div className="w-full space-y-1">
-              <h3 className="text-xl font-semibold text-white">{title}</h3>
-              <p className="text-sm text-white">{cardDescription}</p>
+          <div
+            className={`w-full h-full ${bgCard} bg-no-repeat bg-right bg-contain`}
+          >
+            <div className="relative flex justify-center items-center gap-5 w-full h-full p-6 text-start bg-gray-900/25">
+              <span className="flex items-center justify-center aspect-square rounded-full bg-lime-50 text-5xl text-lime-500">
+                {icon}
+              </span>
+              <div className="w-full space-y-1">
+                <h3 className="text-xl font-semibold text-white">{title}</h3>
+                <p className="text-sm text-white">{cardDescription}</p>
+              </div>
+              <span className="absolute top-3 right-3 text-2xl text-white">
+                <FaAngleRight />
+              </span>
             </div>
-            <span className="absolute top-3 right-3 text-2xl text-white">
-              <FaAngleRight />
-            </span>
           </div>
-        </div>
-      </button>
+        </button>
+      )}
       {serviceModalOpen && (
         <Modal
           isOpen={serviceModalOpen}
