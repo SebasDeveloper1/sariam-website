937fa45fff61e2a4d9884fe29b17e9e629e9079d
Sebastian-Pedroza <sebaspedroza.developer@gmail.com>
2023-07-21 18:42:34 -0500

    :wrench: Add unit test configuration files with Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,8 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module',
-    project: './tsconfig.json',
+    // project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint', 'react', 'prettier'],
   rules: {
