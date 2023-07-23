9bfd323e58e6298d917e9216db075445a02cd0d1
Sebastian-Pedroza <sebaspedroza.developer@gmail.com>
2023-07-22 23:18:24 -0500

    :white_check_mark: Update ContactButton component test

--- __tests__/components/Buttons/ContactButton/ContactButton.test.tsx
+++ __tests__/components/Buttons/ContactButton/ContactButton.test.tsx
@@ -11,34 +11,40 @@ describe('ContactButton Component', () => {
     button = getByRole('link');
   });
 
-  it('renders a button', () => {
-    // Check if the button is in the document
-    expect(button).toBeInTheDocument();
+  describe('Rendering', () => {
+    it('should render a button', () => {
+      // Check if the button is in the document
+      expect(button).toBeInTheDocument();
+    });
+
+    it('should render a button with the correct link attributes', () => {
+      // Check if the button has the correct link attributes
+      expect(button).toHaveAttribute('href', 'https://wa.link/kuu26s');
+      expect(button).toHaveAttribute('target', '_blank');
+      expect(button).toHaveAttribute('rel', 'noopener noreferrer');
+    });
+
+    it('should render a button with the WhatsApp icon', () => {
+      // Check if the button contains the WhatsApp icon element
+      expect(button).toContainElement(document.querySelector('#whatsapp-icon'));
+    });
   });
 
-  it('renders a button with the correct link attributes', () => {
-    // Check if the button has the correct link attributes
-    expect(button).toHaveAttribute('href', 'https://wa.link/kuu26s');
-    expect(button).toHaveAttribute('target', '_blank');
-    expect(button).toHaveAttribute('rel', 'noopener noreferrer');
+  describe('Interaction', () => {
+    it('should redirect to the correct WhatsApp URL on click', () => {
+      // Simulate a click on the button
+      fireEvent.click(button);
+
+      // Since Jest doesn't allow us to directly modify window.location, we cannot test the redirection directly.
+      // You may consider using a library like "jest-localstorage-mock" to mock the window.location behavior and test the redirection.
+    });
   });
 
-  it('renders a button with the WhatsApp icon', () => {
-    // Check if the button contains the WhatsApp icon element
-    expect(button).toContainElement(document.querySelector('#whatsapp-icon'));
-  });
-
-  it('redirects to the correct WhatsApp URL on click', () => {
-    // Simulate a click on the button
-    fireEvent.click(button);
-
-    // Since Jest doesn't allow us to directly modify window.location, we cannot test the redirection directly.
-    // You may consider using a library like "jest-localstorage-mock" to mock the window.location behavior and test the redirection.
-  });
-
-  test('matches snapshot', () => {
-    // Take a snapshot of the rendered component and compare it to a saved snapshot
-    const { asFragment } = render(<ContactButton />);
-    expect(asFragment()).toMatchSnapshot();
+  describe('Snapshot', () => {
+    it('should match the ContactButton component snapshot', () => {
+      // Take a snapshot of the rendered component and compare it to a saved snapshot
+      const { asFragment } = render(<ContactButton />);
+      expect(asFragment()).toMatchSnapshot();
+    });
   });
 });
